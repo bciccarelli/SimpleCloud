f2ead5c214c23ba7554d92ea61792a80306b4e81
AluminumChassis2 <32084215+AluminumChassis@users.noreply.github.com>
2018-12-20 21:54:59 -0700

    basic functionality

--- server.js
+++ server.js
@@ -12,37 +12,40 @@ app.use(function(req, res, next) {
 });
 
 const fs = require('fs');
+const aes = require('aes-js');
 const crypto = require('crypto');
-const password = 'Password used to generate key';
-// Key length is dependent on the algorithm. In this case for aes192, it is
-// 24 bytes (192 bits).
-// Use async `crypto.scrypt()` instead.
-const key = crypto.scryptSync(password, 'salt', 16);
-var message = "hello"
-
-var m = cipher(key,message)
-console.log(m);
-
-console.log(decipher(key,m));
-
+const password = "banana";
+const salt = "salt";
+console.log(encode(password));
+console.log(encode(salt));
+var key = crypto.pbkdf2Sync(encode(password), encode(salt), 100, 16, 'sha512');
+console.log(key)
 app.use(express.static('public'));
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get('/one', function(request, response) {
-  console.log((request.rawHeaders));
+  //console.log((request.rawHeaders));
   const readStream = fs.createReadStream('./img.jpg');
 	readStream.on('data', (chunk) =>{
-	  response.send(cipher(key,chunk.toString('base64')));
+
+	  	response.send(cipher(key,padRight(chunk.toString("base64"))));
 	});
 });
-
+app.get('*', function(req, res){
+  res.send('page not found <a href="one">try this</a>', 404);
+});
+function padRight(message){
+	m = message
+	for(var i = 0; i < (16-(message.length%16));i++){
+		m += " "
+	}
+	return m
+}
 function cipher(key, message) {
-	var iv = crypto.randomBytes(16);
-	iv = iv.toString('hex').slice(0, 16);
-	var mykey = crypto.createCipheriv('aes-128-cbc', key, iv);
-	var mystr = mykey.update(message, 'utf8', 'hex')
-	mystr += mykey.final('hex');
-	return iv+":"+mystr;
+	var iv = crypto.randomFillSync(new Uint8Array(16)); 
+	console.log(iv)
+	var aesCbc = new aes.ModeOfOperation.cbc(key, Array.from(iv));
+    return aes.utils.hex.fromBytes(iv)+":"+aes.utils.hex.fromBytes(aesCbc.encrypt(aes.utils.utf8.toBytes(message)));
 }
 function decipher(key, message) {
 	var iv = message.split(":")[0];
@@ -53,7 +56,38 @@ function decipher(key, message) {
 	mystr += mykey.final('utf8');
 	return mystr;
 }
+function encode(e) {
+	var a = [];
+	for(var i = 0; i < e.length; i++) a.push(e.charCodeAt(i));
+	return new Uint8Array(a);
+}
+function bytesToHexString(bytes)
+{
+    if (!bytes)
+        return null;
+
+    bytes = new Uint8Array(bytes);
+    var hexBytes = [];
+
+    for (var i = 0; i < bytes.length; ++i) {
+        var byteString = bytes[i].toString(16);
+        if (byteString.length < 2)
+            byteString = "0" + byteString;
+        hexBytes.push(byteString);
+    }
+
+    return hexBytes.join("");
+}
 // listen for requests :)
 const listener = app.listen(3000, function() {
     console.log('Listening to port:  ' + 3000);
 });
+function stringToUint(string) {
+
+    var charList = string.split(''),
+        uintArray = [];
+    for (var i = 0; i < charList.length; i++) {
+        uintArray.push(charList[i].charCodeAt(0));
+    }
+    return new Uint8Array(uintArray);
+}
